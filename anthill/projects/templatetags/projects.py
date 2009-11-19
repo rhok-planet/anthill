838179db41b6cac03d34fdc3f8903b1ad9ad24c9
James Turk <james.p.turk@gmail.com>
2009-11-19 15:48:02 -0500

    remove dependency on newsfeed, labs-site specific

--- anthill/projects/templatetags/projects.py
+++ anthill/projects/templatetags/projects.py
@@ -1,20 +1,9 @@
-import datetime
 from django import template
 from django.db.models import Count
-from anthill.utils import get_items_as_tag, chart_from_tags, _extract_chart_params, ChartNode
+from anthill.utils import chart_from_tags, _extract_chart_params, ChartNode
 from anthill.projects.models import Project
-from newsfeed.models import FeedItem
 
 register = template.Library()
-
-@register.tag
-def get_active_projects(parser, token):
-    start_time = datetime.datetime.now() - datetime.timedelta(30)  # month ago
-    most_active = FeedItem.objects.filter(timestamp__gt=start_time).values_list('feed__slug', flat=True).order_by('count').annotate(count=Count('id'))[:5]
-    projects = Project.objects.filter(slug__in=list(most_active))
-    if hasattr(projects, '_gatekeeper'):
-        projects = projects.approved()
-    return get_items_as_tag(token, projects)
 
 @register.tag
 def project_skills_piechart(parser, token):
